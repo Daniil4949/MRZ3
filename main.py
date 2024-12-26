f37a03a558e29f09c998db3ea6a265a391e09734
daniilkimstach <daniilkimstachp@gmail.com>
2024-12-26 19:11:28 +0300

    Updating Jordan network and adding plots

--- main.py
+++ main.py
@@ -64,16 +64,15 @@ def create_dataset(seq, window_size):
 
 if __name__ == '__main__':
     sequence = generate_arithmetic_sequence(1, 3, 30)
-    print("\nИзначальная последовательность:", sequence)
     sequence_norm, mean, std = normalize(sequence)
     window_size = 5
     X, Y = create_dataset(sequence_norm, window_size)
-    print("\nПримеры входных данных (X) и целевых значений (Y):")
+    print("\nПримеры входных данных и  ожидаемых значений:")
     net = JordanNetwork(input_size=1, hidden_size=10, output_size=1, learning_rate=1e-4, alpha=0.01)
     losses = net.train(X, Y, max_epochs=1000, target_loss=1e-8)
 
     predict_steps = window_size
-    mape_values = []  # список для хранения MAPE
+    mape_values = []
 
     for idx, start_idx in enumerate(range(0, len(sequence_norm) - window_size, window_size), 1):
         window = sequence_norm[start_idx: start_idx + window_size]
@@ -81,22 +80,16 @@ if __name__ == '__main__':
         if len(window) < window_size:
             break
 
-        print(f"\nОкно #{idx}: {denormalize(window, mean, std)}")
+        print(f"\nИнтервал (окно) #{idx}: {denormalize(window, mean, std)}")
 
         preds_norm = net.predict(window, predict_steps, window_size)
         preds = denormalize(preds_norm, mean, std)
-        print(f"  Прогноз сети (на {predict_steps} шагов): {preds}")
+        print(f"Прогноз на {predict_steps} шагов: {preds}")
 
         expected = sequence[start_idx + window_size: start_idx + window_size + predict_steps]
-        print(f"  Ожидаемые значения: {expected}")
-
-        if len(expected) < predict_steps:
-            print("  Недостаточно данных для сравнения. Пропуск.")
-            continue
-
-        local_mae = mean_absolute_error(expected, preds)
+        print(f"Ожидаемые значения: {expected}")
         local_mape = mean_absolute_percentage_error(expected, preds)
 
         mape_values.append(local_mape)
-        print(f"  MAE: {local_mae:.2f}, MAPE: {local_mape:.2f}%")
+        print(f"MAPE: {local_mape:.2f}%")
         print(f"\nСредняя абсолютная процентная ошибка (MAPE) по всем окнам: {np.mean(mape_values):.2f}%")
